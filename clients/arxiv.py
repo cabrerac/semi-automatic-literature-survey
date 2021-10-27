c156d8d34427bafcd583a6979bb5ab6339934f76
cabrerac <christiancabrera89@gmail.com>
2021-10-27 19:19:50 +0100

    adding parameters and requirements

---
+++ clients/arxiv.py
@@ -0,0 +1,72 @@
+import urllib.request
+import pandas as pd
+
+api_url = 'http://export.arxiv.org/api/'
+arxiv_fields = {'title': 'ti', 'abstract': 'abs'}
+
+
+def get_papers(domains, interests, keywords, synonyms, fields):
+    a_fields = []
+    for field in fields:
+        a_field = arxiv_fields[field]
+        a_fields.append(a_field)
+    query = create_query(domains, interests, keywords, synonyms, a_fields)
+    query_url = api_url + query
+    raw_xml = urllib.request.urlopen(query_url).read().decode('utf-8')
+    papers = process_raw_data(raw_xml)
+    return papers
+
+
+def create_query(domains, interests, keywords, synonyms, fields):
+    query = ''
+    query_domains = ''
+    for domain in domains:
+        query_domains = query_domains + '<field>:%22' + domain + '%22'
+        if domain in synonyms:
+            domain_synonyms = synonyms[domain]
+            for synonym in domain_synonyms:
+                query_domains = query_domains + '+OR+<field>:%22' + synonym + '%22'
+        query_domains = query_domains + '+OR+'
+    query_domains = '%28' + query_domains + '%29'
+    query_domains = query_domains.replace('+OR+%29', '%29')
+    query = query + query_domains
+
+    query_interests = ''
+    for interest in interests:
+        query_interests = query_interests + '<field>:%22' + interest + '%22'
+        if interest in synonyms:
+            interest_synonyms = synonyms[interest]
+            for synonym in interest_synonyms:
+                query_interests = query_interests + '+OR+<field>:%22' + synonym + '%22'
+        query_interests = query_interests + '+OR+'
+    query_interests = '%28' + query_interests + '%29'
+    query_interests = query_interests.replace('+OR+%29', '%29')
+    if len(query) > 0 and len(query_interests) > 0:
+        query = query + '+AND+' + query_interests
+    if len(query) == 0 and len(query_interests) > 0:
+        query = query + query_interests
+
+    query_keywords = ''
+    for keyword in keywords:
+        query_keywords = query_keywords + '<field>:%22' + keyword + '%22+OR+'
+    query_keywords = '%28' + query_keywords + '%29'
+    query_keywords = query_keywords.replace('+OR+%29', '%29')
+    if len(query) > 0 and len(query_keywords) > 0:
+        query = query + '+AND+' + query_keywords
+    if len(query) == 0 and len(query_keywords) > 0:
+        query = query + query_keywords
+
+    query = '%28' + query + '%29'
+    query = query.replace(' ', '+')
+    qf = ''
+    for field in fields:
+        qf = qf + query.replace('<field>',field) + '+OR+'
+    query = qf[:-4]
+    query = 'query?search_query=' + query + '&sortBy=lastUpdatedDate&sortOrder=ascending&max_results=5000'
+    print(query)
+    return query
+
+
+def process_raw_data(raw_xml):
+    papers = pd.read_xml(raw_xml, xpath='//feed:entry', namespaces={"feed": "http://www.w3.org/2005/Atom"})
+    return papers
--- main.py
+++ main.py
@@ -1,36 +1,21 @@
-import urllib.request
-import xmltodict, json
+from clients import arxiv
+import yaml
 
-domains = ['self-driving cars']
-interests = ['machine learning', 'artificial intelligence']
-keywords = ['deployment', 'real world deployment']
-synonyms = {'self-driving cars': ['autonomous car', 'driverless car', 'unmanned car']}
-
+with open(r'parameters.yaml') as file:
+    # The FullLoader parameter handles the conversion from YAML
+    # scalar values to Python the dictionary format
+    parameters = yaml.load(file, Loader=yaml.FullLoader)
+domains = parameters['domains']
+interests = parameters['interests']
+keywords = parameters['keywords']
+fields = parameters['fields']
+synonyms = {}
 for domain in domains:
-    title = '%28ti:' + domain
-    abstract = '%28abs' + domain
-    for synonym in synonyms[domain]:
-        title = title + '+OR+ti:' + synonym
-        abstract = abstract + '+OR+abs:' + synonym
-    title = title + '%29+AND+%28ti:'
-    abstract = abstract + '%29+AND+%28ti:'
-    for interest in interests:
-        title = title + interest + '+OR+ti:'
-        abstract = abstract + interest + '+OR+abs:'
-    title = title + '%29'
-    abstract = abstract + '%29'
-    title = title.replace('+OR+ti:%29','%29')
-    abstract = abstract.replace('+OR+abs:%29', '%29')
-    for keyword in keywords:
-        title = title + '+AND+ti:' + keyword
-        abstract = abstract + '+AND+abs:' + keyword
-        search_query = '%28' + title + '%29+OR+%28' + abstract + '%29'
-        search_query = search_query.replace(' ', '%20')
-        print(search_query)
-        # Getting papers from arXiv
-        url = 'http://export.arxiv.org/api/query?search_query='+search_query
-        data = urllib.request.urlopen(url)
-        print(data.read().decode('utf-8'))
+    synonyms[domain] = parameters[domain]
+for interest in interests:
+    synonyms[interest] = parameters[interest]
+arxiv_papers = arxiv.get_papers(domains, interests, keywords, synonyms, fields)
+print(arxiv_papers)
 
 
 
