db2dc2c5f1af111560f75dc7efe928b10e8655de
cabrerac <christiancabrera89@gmail.com>
2021-10-28 20:02:16 +0100

    adding xplore api client

--- clients/ieeexplore.py
+++ clients/ieeexplore.py
@@ -1,17 +1,80 @@
+import time
+
 from .xplore.xploreapi import XPLORE
+import json
+import pandas as pd
 
+api_access = 'xmknyxp8j436aun5c5tj7g75'
+ieee_fields = {'title': 'article_title', 'abstract': 'abstract'}
+ieee_types = {'books': 'Books', 'courses':'Courses', 'conferences': 'Conferences',
+              'early access': 'Early Access', 'journals': 'Journals', 'standards': 'Standards'}
 
-def get_papers(domains, interests, keywords, synonyms, fields):
-    a_fields = []
-    query = create_query(domains, interests, keywords, synonyms, a_fields)
-    client = XPLORE('api_access_key')
-    client.abstractText(query)
-    papers = client.callAPI()
+def get_papers(domains, interests, keywords, synonyms, fields, types):
+    i_fields = []
+    for field in fields:
+        i_fields.append(ieee_fields[field])
+    i_types = []
+    for type in types:
+        i_types.append(ieee_types[type])
+    queries = create_query(domains, interests, keywords, synonyms, i_fields)
+    all_papers = []
+    for query in queries:
+        for field in i_fields:
+            for type in i_types:
+                client = XPLORE(api_access)
+                client.searchField(field, query)
+                client.resultsFilter("content_type", type)
+                client.maximumResults(200)
+                print(query)
+                print(field)
+                print(type)
+                raw_papers = client.callAPI()
+                all_papers = process_raw_data(all_papers, raw_papers)
+                time.sleep(10)
+    print(all_papers)
+    papers = all_papers.drop_duplicates(subset=['doi'])
+    print(papers)
     return papers
 
 
 def create_query(domains, interests, keywords, synonyms, a_fields):
-    return ''
+    queries = []
+    for keyword in keywords:
+        query = '"' + keyword + '"'
+
+        query_domains = ''
+        for domain in domains:
+            query_domains = query_domains + '"' + domain + '"' + 'OR'
+            domain_synonyms = synonyms[domain]
+            for synonym in domain_synonyms:
+                query_domains = query_domains + '"' + synonym + '"' + 'OR'
+        if len(query_domains) > 0:
+            query = query + 'AND(' + query_domains + ')'
+        query = query.replace('OR)', ')')
+
+        query_interests = ''
+        for interest in interests:
+            query_interests = query_interests + '"' + interest + '"' + 'OR'
+            interest_synonyms = synonyms[interest]
+            for synonym in interest_synonyms:
+                query_interests = query_interests + '"' + synonym + '"' + 'OR'
+        if len(query_interests) > 0:
+            query = query + 'AND(' + query_interests + ')'
+        query = query.replace('OR)', ')')
+        queries.append(query)
+    return queries
+
+
+def process_raw_data(papers, raw_papers):
+    raw_json = json.loads(raw_papers)
+    print("Results: " + str(raw_json['total_records']))
+    if 'articles' in raw_json:
+        df = pd.json_normalize(raw_json['articles'])
+        if len(papers)==0:
+            papers = df
+        else:
+            papers = papers.append(df)
+    return papers
 
 
 
--- main.py
+++ main.py
@@ -10,16 +10,17 @@ domains = parameters['domains']
 interests = parameters['interests']
 keywords = parameters['keywords']
 fields = parameters['fields']
+types = parameters['types']
 synonyms = {}
 for domain in domains:
     synonyms[domain] = parameters[domain]
 for interest in interests:
     synonyms[interest] = parameters[interest]
 
-ieee_explore_papers = ieeexplore.get_papers(domains, interests, keywords, synonyms, fields)
-print(ieee_explore_papers)
-arxiv_papers = arxiv.get_papers(domains, interests, keywords, synonyms, fields)
-print(arxiv_papers)
+#ieee_explore_papers = ieeexplore.get_papers(domains, interests, keywords, synonyms, fields, types)
+#print(ieee_explore_papers)
+#arxiv_papers = arxiv.get_papers(domains, interests, keywords, synonyms, fields, types)
+#print(arxiv_papers)
 
 
 
