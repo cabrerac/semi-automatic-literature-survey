edf0d9d8663f90bb1239ef10ce25b0d7d5395cbc
cabrerac <christiancabrera89@gmail.com>
2021-11-01 14:21:46 +0000

    ieeexplore client and main

--- clients/ieeexplore.py
+++ clients/ieeexplore.py
@@ -1,80 +1,74 @@
 import time
-from .xplore.xploreapi import XPLORE
+from .apis.xploreapi import XPLORE
+from .apis.generic import Generic
 import json
 import pandas as pd
 
 api_access = 'xmknyxp8j436aun5c5tj7g75'
-ieee_fields = {'title': 'article_title', 'abstract': 'abstract'}
-ieee_types = {'books': 'Books', 'courses':'Courses', 'conferences': 'Conferences',
+max_papers = 200
+client_fields = {'title': 'article_title', 'abstract': 'abstract'}
+client_types = {'books': 'Books', 'courses':'Courses', 'conferences': 'Conferences',
               'early access': 'Early Access', 'journals': 'Journals', 'standards': 'Standards'}
+database = 'ieeexplore'
+client = Generic()
 
 
-def get_papers(domains, interests, keywords, synonyms, fields, types):
-    i_fields = []
+def get_papers(domains, interests, keywords, synonyms, fields, types, retrieve):
+    stats = []
+    papers = []
+    c_fields = []
     for field in fields:
-        i_fields.append(ieee_fields[field])
-    i_types = []
-    for type in types:
-        i_types.append(ieee_types[type])
-    queries = create_query(domains, interests, keywords, synonyms, i_fields)
-    all_papers = []
+        if field in client_fields:
+            c_fields.append(client_fields[field])
+    c_types = []
+    for t in types:
+        if t in client_types:
+            c_types.append(client_types[t])
+    parameters = {'domains': domains, 'interests': interests, 'keywords': keywords, 'synonyms': synonyms,
+                  'fields': c_fields, 'types': c_types}
+    if len(keywords) > 0:
+        queries = client.ieeexplore_query(parameters, 'keywords')
+    else:
+        queries = client.ieeexplore_query(parameters, 'domains')
+    total_requests = len(queries) * len(c_fields) * len(c_types)
+    print(str(total_requests))
+    current_request = 1
     for query in queries:
-        for field in i_fields:
-            for type in i_types:
-                client = XPLORE(api_access)
-                client.searchField(field, query)
-                client.resultsFilter("content_type", type)
-                client.maximumResults(200)
-                print(query)
-                print(field)
-                print(type)
-                raw_papers = client.callAPI()
-                all_papers = process_raw_data(all_papers, raw_papers)
+        for field in c_fields:
+            for t in c_types:
+                print(str(current_request))
+                raw_papers = request(query, field, t)
+                if raw_papers != {}:
+                    stats, papers = process_raw_data(papers, raw_papers, retrieve)
                 time.sleep(10)
-    print(all_papers)
-    papers = all_papers.drop_duplicates(subset=['doi'])
-    print(papers)
-    return papers
+                current_request = current_request + 1
+    papers = papers.drop_duplicates(subset=['doi'])
+    if retrieve:
+        papers = client.filterByField(papers, 'abstract', keywords)
+    stats[2] = len(papers.index)
+    stats.append(domains[0])
+    #papers = client.filterByField(papers, 'abstract', keywords)
+    return stats, papers
 
 
-def create_query(domains, interests, keywords, synonyms, a_fields):
-    queries = []
-    for keyword in keywords:
-        query = '"' + keyword + '"'
-
-        query_domains = ''
-        for domain in domains:
-            query_domains = query_domains + '"' + domain + '"' + 'OR'
-            domain_synonyms = synonyms[domain]
-            for synonym in domain_synonyms:
-                query_domains = query_domains + '"' + synonym + '"' + 'OR'
-        if len(query_domains) > 0:
-            query = query + 'AND(' + query_domains + ')'
-        query = query.replace('OR)', ')')
-
-        query_interests = ''
-        for interest in interests:
-            query_interests = query_interests + '"' + interest + '"' + 'OR'
-            interest_synonyms = synonyms[interest]
-            for synonym in interest_synonyms:
-                query_interests = query_interests + '"' + synonym + '"' + 'OR'
-        if len(query_interests) > 0:
-            query = query + 'AND(' + query_interests + ')'
-        query = query.replace('OR)', ')')
-        queries.append(query)
-    return queries
+def request(query, field, t):
+    client_ieee = XPLORE(api_access)
+    client_ieee.searchField(field, query)
+    client_ieee.resultsFilter("content_type", t)
+    client_ieee.maximumResults(max_papers)
+    raw_papers = client_ieee.callAPI()
+    return raw_papers
 
 
-def process_raw_data(papers, raw_papers):
+def process_raw_data(papers, raw_papers, retrieve):
     raw_json = json.loads(raw_papers)
-    print("Results: " + str(raw_json['total_records']))
+    total = raw_json['total_records']
     if 'articles' in raw_json:
         df = pd.json_normalize(raw_json['articles'])
-        if len(papers)==0:
+        if len(papers) == 0:
             papers = df
         else:
             papers = papers.append(df)
-    return papers
-
-
-
+        papers['database'] = database
+    stats = [database, retrieve, total]
+    return stats, papers
--- clients/springer.py
+++ clients/springer.py
@@ -2,101 +2,77 @@ import time
 import urllib.request
 import pandas as pd
 import json
+from .apis.generic import Generic
 
-api_url = 'http://api.springernature.com/meta/v2/json'
-springer_fields = {'title': 'title', 'abstract': 'keyword'}
+api_url = 'http://api.springernature.com/meta/v2/json?q='
 api_access = 'd5a276d7379bb80d31ec868f7e34e386'
+start = 1
+max_papers = 100
+client_fields = {'title': 'title', 'abstract': 'keyword'}
+database = 'springer'
+client = Generic()
 
 
-def get_papers(domains, interests, keywords, synonyms, fields, types):
-    s_fields = []
+def get_papers(domains, interests, keywords, synonyms, fields, types, retrieve):
+    c_fields = []
+    papers = []
     for field in fields:
-        if field in springer_fields:
-            s_fields.append(springer_fields[field])
-    all_papers = []
-    query = create_query(domains, interests, keywords, synonyms, s_fields)
-    start = 1
-    query_url = api_url + query + '&s='+str(start)+'&p=100&api_key=' + api_access
-    raw_result = urllib.request.urlopen(query_url).read().decode('utf-8')
-    json_result = json.loads(raw_result)
-    total = json_result['result'][0]['total']
-    all_papers = process_raw_data(all_papers, json_result)
-    print(total)
-    if int(total) > 100:
-        times = int(int(total)/100) - 1
-        print(times)
+        if field in client_fields:
+            c_fields.append(client_fields[field])
+    parameters = {'domains': domains, 'synonyms': synonyms,
+                  'fields': c_fields, 'types': types}
+    req = create_request(parameters)
+    raw_papers = client.request(req, 'get', {})
+    stats, total, papers = process_raw_papers(papers, raw_papers, retrieve)
+    if total > 100 and retrieve:
+        times = int(total/100) - 1
         mod = int(total) % 100
         if mod > 0:
             times = times + 1
-        print(times)
-        for t in range(1, times+1):
+        for t in range(1, times + 1):
+            print(str(t))
             time.sleep(5)
-            start = (100*t) + 1
-            print(str(start))
-            query_url = api_url + query + '&s=' + str(start) + '&p=100&api_key=' + api_access
-            raw_result = urllib.request.urlopen(query_url).read().decode('utf-8')
-            json_result = json.loads(raw_result)
-            all_papers = process_raw_data(all_papers, json_result)
-
-    print(all_papers)
-    return all_papers
-
-
-def create_query(domains, interests, keywords, synonyms, fields):
-    query = ''
-    query_domains = ''
-    for domain in domains:
-        query_domains = query_domains + '<field>:%22' + domain + '%22'
-        if domain in synonyms:
-            domain_synonyms = synonyms[domain]
-            for synonym in domain_synonyms:
-                query_domains = query_domains + ' OR <field>:%22' + synonym + '%22'
-        query_domains = query_domains + ' OR '
-    query_domains = '(' + query_domains + ')'
-    query_domains = query_domains.replace(' OR )', ')')
-    query = query + query_domains
-
-    query_interests = ''
-    for interest in interests:
-        query_interests = query_interests + '<field>:%22' + interest + '%22'
-        if interest in synonyms:
-            interest_synonyms = synonyms[interest]
-            for synonym in interest_synonyms:
-                query_interests = query_interests + ' OR <field>:%22' + synonym + '%22'
-        query_interests = query_interests + ' OR '
-    query_interests = '(' + query_interests + ')'
-    query_interests = query_interests.replace(' OR )', ')')
-    if len(query) > 0 and len(query_interests) > 0:
-        query = query + ' AND ' + query_interests
-    if len(query) == 0 and len(query_interests) > 0:
-        query = query + query_interests
-
-    """query_keywords = ''
-    for keyword in keywords:
-        query_keywords = query_keywords + '<field>:%22' + keyword + '%22 OR '
-    query_keywords = '(' + query_keywords + ')'
-    query_keywords = query_keywords.replace(' OR )', ')')
-    if len(query) > 0 and len(query_keywords) > 0:
-        query = query + ' AND ' + query_keywords
-    if len(query) == 0 and len(query_keywords) > 0:
-        query = query + query_keywords"""
-
-    query = '(' + query + ')'
-    qf = ''
-    for field in fields:
-        qf = qf + query.replace('<field>', field) + ' OR '
-    query = qf[:-4]
-    query = query.replace(' ', '%20')
-    query = '?q=' + query
-    print(query)
-    return query
+            global start
+            start = (max_papers * t) + 1
+            req = create_request(parameters)
+            raw_papers = client.request(req, 'get', {})
+            if raw_papers != {}:
+                stats, total, papers = process_raw_papers(papers, raw_papers, retrieve)
+    if retrieve:
+        filters_interest = []
+        for interest in interests:
+            filters_interest.append(interest)
+            synonyms_interest = synonyms[interest]
+            for synonym in synonyms_interest:
+                filters_interest.append(synonym)
+        papers = client.filterByField(papers, 'abstract', filters_interest)
+        papers = client.filterByField(papers, 'abstract', keywords)
+        stats[2] = len(papers.index)
+        papers = papers.drop(columns=['creators', 'bookEditors', 'openaccess', 'printIsbn', 'electronicIsbn',
+                                      'isbn', 'genre', 'copyright', 'conferenceInfo', 'issn', 'eIssn', 'volume',
+                                      'number', 'issueType', 'topicalCollection', 'startingPage', 'endingPage',
+                                      'journalId', 'printDate', 'coverDate', 'keyword'])
+    stats.append(domains[0])
+    return stats, papers
 
 
-def process_raw_data(papers, json_results):
+def create_request(parameters):
+    req = api_url
+    req = req + client.default_query(parameters)
+    req = req + '&s='+str(start)+'&p='+str(max_papers)+'&api_key=' + api_access
+    req = req.replace('%28', '(').replace('%29', ')').replace('+', '%20')
+    return req
+
+
+def process_raw_papers(papers, raw_papers, retrieve):
+    json_results = json.loads(raw_papers)
+    total = int(json_results['result'][0]['total'])
     if 'records' in json_results:
         df = pd.json_normalize(json_results['records'])
         if len(papers) == 0:
             papers = df
         else:
             papers = papers.append(df)
-    return papers
+    papers['database'] = database
+    stats = [database, retrieve, int(total)]
+    return stats, total, papers
