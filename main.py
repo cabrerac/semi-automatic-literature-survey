bd0fb5c6fe1dc34c80adc73ddd643091900fc817
cabrerac <christiancabrera89@gmail.com>
2021-11-01 17:06:38 +0000

    refactoring

--- main.py
+++ main.py
@@ -24,54 +24,59 @@ def read_parameters(file_name):
     return domains, interests, keywords, synonyms, fields, types
 
 
-def get_papers(domains, interests, keywords, synonyms, fields, types, retrieve):
-    stats = []
-    papers = []
-    for domain in domains:
-        print("Requesting IEEE Xplore for " + domain+ " related papers...")
-        stats_ieee, ieee_explore_papers = ieeexplore.get_papers([domain], interests, keywords, synonyms, fields, types, retrieve)
-        print(stats_ieee)
+def get_papers(domain, interests, keywords, synonyms, fields, types):
 
-        #print("Requesting Springer for " + domain + " related papers...")
-        #stats_springer, springer_papers = springer.get_papers([domain], interests, keywords, synonyms, fields, types, retrieve)
-        #springer_papers.abstract.str.encode('utf-8')
-        #print(stats_springer)
+    #print("Requesting ArXiv for " + domain + " related papers...")
+    #arxiv.get_papers(domain, interests, keywords, synonyms, fields, types)
 
-        #print("Requesting ArXiv for " + domain + " related papers...")
-        #stats_arxiv, arxiv_papers = arxiv.get_papers([domain], interests, keywords, synonyms, fields, types, retrieve)
-        #print(stats_arxiv)
+    print("Requesting Springer for " + domain + " related papers...")
+    springer.get_papers(domain, interests, keywords, synonyms, fields, types)
 
-        # Collecting stats in dataframe
-        stats.append(stats_ieee)
-        #stats.append(stats_springer)
-        #stats.append(stats_arxiv)
-        stats = pd.DataFrame(stats, columns=['database', 'filtered', 'papers', 'domain'])
-        if retrieve:
-            with open('ieee.csv', 'a', newline='', encoding='utf-8') as f:
-                ieee_explore_papers.to_csv(f, encoding='utf-8', index=False, header=f.tell() == 0)
-            #with open('springer.csv', 'a', newline='', encoding='utf-8') as f:
-                #springer_papers.to_csv(f, encoding='utf-8', index=False, header=f.tell() == 0)
-            #with open('arxiv.csv', 'a', newline='', encoding='utf-8') as f:
-                #arxiv_papers.to_csv(f, encoding='utf-8', index=False, header=f.tell() == 0)
-            with open('stats_filtered.csv', 'a', newline='', encoding='utf-8') as f:
-                stats.to_csv(f, encoding='utf-8', index=False, header=f.tell() == 0)
-        else:
-            with open('stats_all.csv', 'a', newline='', encoding='utf-8') as f:
-                stats.to_csv(f, encoding='utf-8', index=False, header=f.tell() == 0)
+    #print("Requesting IEEE Xplore for " + domain + " related papers...")
+    #stats_ieee, ieee_explore_papers = ieeexplore.get_papers([domain], interests, keywords, synonyms, fields, types, retrieve)
+    #print(stats_ieee)
+
+    #print("Requesting Springer for " + domain + " related papers...")
+    #stats_springer, springer_papers = springer.get_papers([domain], interests, keywords, synonyms, fields, types, retrieve)
+    #springer_papers.abstract.str.encode('utf-8')
+    #print(stats_springer)
+
+    #print("Requesting ArXiv for " + domain + " related papers...")
+    #stats_arxiv, arxiv_papers = arxiv.get_papers([domain], interests, keywords, synonyms, fields, types, retrieve)
+    #print(stats_arxiv)
+
+    # Collecting stats in dataframe
+    # stats.append(stats_ieee)
+    #stats.append(stats_springer)
+    #stats.append(stats_arxiv)
+    """stats = pd.DataFrame(stats, columns=['database', 'filtered', 'papers', 'domain'])
+    if retrieve:
+        with open('ieee.csv', 'a', newline='', encoding='utf-8') as f: 
+           ieee_explore_papers.to_csv(f, encoding='utf-8', index=False, header=f.tell() == 0)
+        #with open('springer.csv', 'a', newline='', encoding='utf-8') as f:
+            #springer_papers.to_csv(f, encoding='utf-8', index=False, header=f.tell() == 0)
+        #with open('arxiv.csv', 'a', newline='', encoding='utf-8') as f:
+            #arxiv_papers.to_csv(f, encoding='utf-8', index=False, header=f.tell() == 0)
+        with open('stats_filtered.csv', 'a', newline='', encoding='utf-8') as f:
+            stats.to_csv(f, encoding='utf-8', index=False, header=f.tell() == 0)
+    else:
+        with open('stats_all.csv', 'a', newline='', encoding='utf-8') as f:
+            stats.to_csv(f, encoding='utf-8', index=False, header=f.tell() == 0)"""
 
 
-#print('Reading parameters file...')
-#domains, interests, keywords, synonyms, fields, types = read_parameters('parameters.yaml')
+print('Reading parameters file...')
+domains, interests, keywords, synonyms, fields, types = read_parameters('parameters.yaml')
 
-#print('Getting all stats...')
-#get_papers(domains, [], [], synonyms, fields, types, False)
+for domain in domains:
+    print('Getting all papers for domain: ' + domain + '...')
+    get_papers(domain, interests, [], synonyms, fields, types)
 
 #print('Getting filtered stats and papers...')
 #get_papers(domains, interests, keywords, synonyms, fields, types, True)
 
-stats_all = pd.read_csv('stats_all.csv')
-stats_filtered = pd.read_csv('stats_filtered.csv')
-stats_all = stats_all.append(stats_filtered)
-fig, ax = plt.subplots()
-ax = sns.barplot(x="database", y="papers", hue="filtered", data=stats_all)
-plt.savefig('all_stats.png')
+#stats_all = pd.read_csv('stats_all.csv')
+#stats_filtered = pd.read_csv('stats_filtered.csv')
+#stats_all = stats_all.append(stats_filtered)
+#fig, ax = plt.subplots()
+#ax = sns.barplot(x="database", y="papers", hue="filtered", data=stats_all)
+#plt.savefig('all_stats.png')
