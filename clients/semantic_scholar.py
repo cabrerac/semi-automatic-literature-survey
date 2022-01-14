9403353f0546a810d649ee8e98a8ae35e842a112
cabrerac <christiancabrera89@gmail.com>
2022-01-14 21:35:43 +0000

    snowballing

--- clients/semantic_scholar.py
+++ clients/semantic_scholar.py
@@ -10,6 +10,8 @@ client = Generic()
 database = 'semantic_scholar'
 api_url = 'http://api.semanticscholar.org/graph/v1/paper/search?query=<query>&offset=<offset>&limit=<max_papers>&' \
           'fields=title,abstract,url,year,venue,externalIds'
+citations_url = 'https://api.semanticscholar.org/graph/v1/paper/{paper_id}/citations?fields=title,abstract,url,year,' \
+                'venue&offset=<offset>&limit=<max_papers>'
 max_papers = 100
 format = 'utf-8'
 
@@ -39,6 +41,45 @@ def get_papers(domain, interests, keywords, synonyms, fields, types):
                     util.save(file_name, papers, format)
 
 
+def get_citations():
+    not_found = []
+    file_name = 'citations_papers.csv'
+    papers = pd.read_csv('./papers/final_papers.csv')
+    for index, row in papers.iterrows():
+        paper_id = row['doi']
+        if paper_id == '':
+            paper_id = row['url']
+        if paper_id != '':
+            req = citations_url.replace('{paper_id}', str(paper_id))
+            req = req.replace('<offset>', '0').replace('<max_papers>', str(max_papers))
+            raw_citations = client.request(req, 'retrieve', {})
+            if raw_citations == {}:
+                not_found.append(row['title'])
+                print(row['title'])
+            papers, next = process_raw_citations(raw_citations)
+            if len(papers) != 0:
+                util.save(file_name, papers, format)
+            else:
+                not_found.append(row['title'])
+                print(row['title'])
+            while next != -1:
+                time.sleep(5)
+                print(str(next))
+                req = citations_url.replace('{paper_id}', str(paper_id)).replace('<offset>', str(next))
+                req = req.replace('<max_papers>', str(max_papers))
+                raw_citations = client.request(req, 'retrieve', {})
+                if raw_citations == {}:
+                    not_found.append(row['title'])
+                    print(row['title'])
+                papers, next = process_raw_citations(raw_citations)
+                if len(papers) != 0:
+                    util.save(file_name, papers, format)
+            time.sleep(5)
+        else:
+            not_found.append(row['title'])
+            print(row['title'])
+    util.save('not_found.csv', not_found, format)
+
 def create_request(parameters):
     queries = []
     synonyms = parameters['synonyms'][parameters['domains'][0]]
@@ -67,3 +108,14 @@ def process_raw_papers(raw_papers):
                                   'externalIds.PubMed', 'externalIds.ArXiv'], errors='ignore')
     papers['database'] = database
     return total, papers, next
+
+
+def process_raw_citations(raw_citations):
+    next = -1
+    papers = {}
+    if raw_citations != {}:
+        raw_json = json.loads(raw_citations)
+        if 'next' in raw_json:
+            next = raw_json['next']
+        papers = pd.json_normalize(raw_json['data'])
+    return papers, next
--- main.py
+++ main.py
@@ -21,23 +21,51 @@ domains, interests, keywords, synonyms, fields, types, databases = util.read_par
 
 # 4. Filtering papers by abstract
 #print('4. Filtering papers by abstract...')
-#retrieve.filter_papers(keywords)
+#retrieve.filter_papers(keywords, './papers/preprocessed_papers.csv', 'filtered_papers.csv')
 
 
 # 5. Getting papers to check based on semantic analysis
 #print('5. Getting to check papers...')
-#semantic_analyser.get_to_check_papers(keywords)
+#semantic_analyser.get_to_check_papers(keywords, './papers/filtered_papers.csv', 'to_check_papers.csv')
 
 # 6. Manual filtering of papers to check
 #print('6. Manual filtering of papers to check...')
-#manual.manual_filter_by_abstract()
+#manual.manual_filter_by_abstract('./papers/to_check_papers.csv', 'filtered_by_abstract.csv')
 
 
 # 7. Manual filtering by full paper
 #print('7. Manual filtering by full paper...')
-#manual.manual_filter_by_full_text()
-#manual.remove_repeated()
+#manual.manual_filter_by_full_text('./papers/filtered_by_abstract.csv', 'filtered_by_full_text.csv')
+#manual.remove_repeated('./papers/filtered_by_full_text.csv')
 
-# 8. Plot results
-print('8. Plotting results...')
+
+# 8. Snowballing
+#print('8. Snowballing...')
+#retrieve.get_citations()
+
+# 9. Filtering citation papers by abstract
+#print('9. Filtering citation papers by abstract...')
+#retrieve.filter_papers(keywords, './papers/citations_papers.csv', 'filtered_citations_papers.csv')
+
+# 10. Getting citations papers to check based on semantic analysis
+#print('10. Getting to check citations papers...')
+#semantic_analyser.get_to_check_papers(keywords, './papers/filtered_citations_papers.csv',
+#                                      'to_check_citations_papers.csv')
+
+# 11. Manual filtering of citations papers to check
+print('11. Manual filtering of citations papers to check...')
+manual.manual_filter_by_abstract('./papers/to_check_citations_papers.csv', 'filtered_by_abstract_citations.csv')
+
+
+# 12. Manual filtering by full citations paper
+print('12. Manual filtering by full paper...')
+manual.manual_filter_by_full_text('./papers/filtered_by_abstract_citations.csv', 'filtered_by_full_text_citations.csv')
+manual.remove_repeated('./papers/filtered_by_full_text_citations.csv')
+
+# 13. Merge final papers with citations papers
+print('13. Merging final papers with citations papers...')
+
+
+# 14. Plot results
+print('14. Plotting results...')
 util.plot()
