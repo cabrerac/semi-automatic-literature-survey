4df6f17ac2954f57f212adbe6c10de506ecd2cfc
cabrerac <chc79@cam.ac.uk>
2022-10-25 12:59:18 +0100

    final list

--- analysis/semantic_analyser.py
+++ analysis/semantic_analyser.py
@@ -36,34 +36,44 @@ def lbl2vec(keywords, folder_name, search_date, step):
                 index = index + 1
                 labels.append(label)
         labels = pd.DataFrame(labels)
-
-        papers['tagged_abstract'] = papers.apply(lambda row: TaggedDocument(tokenize(row['abstract']), [str(row.name)]), axis=1)
-        papers['abstract_key'] = papers.index.astype(str)
-        lbl2vec_model = Lbl2Vec(keywords_list=list(labels['keywords']), tagged_documents=papers['tagged_abstract'],
-                                label_names=list(labels['class_name']), similarity_threshold=1.0,
-                                min_num_docs=500, epochs=10)
-        lbl2vec_model.fit()
-        model_docs_lbl_similarities = lbl2vec_model.predict_model_docs()
-        papers = papers.merge(model_docs_lbl_similarities, left_on='abstract_key', right_on='doc_key')
-        included_classes = []
-        for cls in classes:
-            for key, items in cls.items():
-                if key not in excluded_classes:
-                    included_classes.append(key)
-        papers = papers.loc[papers['most_similar_label'].isin(included_classes)]
-        papers = papers.drop(['tagged_abstract', 'abstract_key', 'most_similar_label', 'highest_similarity_score',
-                              'doc_key'], axis=1)
-        columns_to_drop = included_classes
-        for cls in excluded_classes:
-            columns_to_drop.append(cls)
-        papers = papers.drop(columns_to_drop, axis=1)
-        papers['id'] = list(range(1, len(papers) + 1))
-        papers['id'] = papers.index.astype(str)
-        papers['type'] = 'to_check'
-        papers['status'] = 'unknown'
-        with open('./papers/' + folder_name + '/' + str(search_date).replace('-', '_') + '/' + str(step) +
-                  '_semantic_filtered_papers.csv', 'a+', newline='', encoding=fr) as f:
-            papers.to_csv(f, encoding=fr, index=False, header=f.tell() == 0)
+        if len(papers.index) <= 100:
+            papers['id'] = list(range(1, len(papers) + 1))
+            papers['id'] = papers.index.astype(str)
+            papers['type'] = 'to_check'
+            papers['status'] = 'unknown'
+            with open('./papers/' + folder_name + '/' + str(search_date).replace('-', '_') + '/' + str(step) +
+                      '_semantic_filtered_papers.csv', 'a+', newline='', encoding=fr) as f:
+                papers.to_csv(f, encoding=fr, index=False, header=f.tell() == 0)
+        else:
+            min_num_docs = 500
+            min_count = 50
+            papers['tagged_abstract'] = papers.apply(lambda row: TaggedDocument(tokenize(row['abstract']), [str(row.name)]), axis=1)
+            papers['abstract_key'] = papers.index.astype(str)
+            lbl2vec_model = Lbl2Vec(keywords_list=list(labels['keywords']), tagged_documents=papers['tagged_abstract'],
+                                    label_names=list(labels['class_name']), similarity_threshold=1.0,
+                                    min_num_docs=min_num_docs, epochs=10, min_count=min_count)
+            lbl2vec_model.fit()
+            model_docs_lbl_similarities = lbl2vec_model.predict_model_docs()
+            papers = papers.merge(model_docs_lbl_similarities, left_on='abstract_key', right_on='doc_key')
+            included_classes = []
+            for cls in classes:
+                for key, items in cls.items():
+                    if key not in excluded_classes:
+                        included_classes.append(key)
+            papers = papers.loc[papers['most_similar_label'].isin(included_classes)]
+            papers = papers.drop(['tagged_abstract', 'abstract_key', 'most_similar_label', 'highest_similarity_score',
+                                  'doc_key'], axis=1)
+            columns_to_drop = included_classes
+            for cls in excluded_classes:
+                columns_to_drop.append(cls)
+            papers = papers.drop(columns_to_drop, axis=1)
+            papers['id'] = list(range(1, len(papers) + 1))
+            papers['id'] = papers.index.astype(str)
+            papers['type'] = 'to_check'
+            papers['status'] = 'unknown'
+            with open('./papers/' + folder_name + '/' + str(search_date).replace('-', '_') + '/' + str(step) +
+                      '_semantic_filtered_papers.csv', 'a+', newline='', encoding=fr) as f:
+                papers.to_csv(f, encoding=fr, index=False, header=f.tell() == 0)
         util.clean_papers(semantic_filtered_file_name)
     return semantic_filtered_file_name
 
